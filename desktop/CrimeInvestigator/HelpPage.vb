99d082f1466931e8ac766b4730e73bb05fa7fb76
ibroheem <movichtech@gmail.com>
2016-04-03 00:05:21 +0100

    Deleted desktop folder

--- desktop/CrimeInvestigator/HelpPage.vb
+++
@@ -1,3 +0,0 @@
-Public Class HelpPage
-
-End Class
--- desktop/CrimeInvestigator/OnlineCheck.vb
+++
@@ -1,6 +0,0 @@
-Public Class OnlineCheck
-
-    Private Sub button_test_connection_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles button_test_connection.Click
-        '
-    End Sub
-End Class
